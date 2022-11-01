eb1f16a6930d3e4d2895e5ecfe59a826128a5acd
Luan Kenzley <luankenzley@hotmail.com>
2022-10-31 22:38:45 -0300

    implementando novos verbos

--- src/main/java/com/devdojo/controller/AnimeController.java
+++ src/main/java/com/devdojo/controller/AnimeController.java
@@ -1,27 +1,45 @@
 package com.devdojo.controller;
 
 import com.devdojo.domain.Anime;
+import com.devdojo.service.AnimeService;
 import com.devdojo.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/anime")
+@RequestMapping("/animes")
 @Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
 
     private final DateUtil dateUtil;
+    private final AnimeService animeService;
 
     @GetMapping(path = "/list")
-    public List<Anime> listAnime() {
+    public ResponseEntity<List<Anime>> listAnime() {
         log.info(dateUtil.formatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("Boku No Hero"), new Anime("Berserk"));
+        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id) {
+        return ResponseEntity.ok(animeService.findById(id));
+    }
+
+    @PostMapping(path = "/new")
+    public ResponseEntity<Anime> saveNewAnime(@RequestBody Anime anime) {
+       return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/devdojo/repository/AnimeRepository.java
+++ src/main/java/com/devdojo/repository/AnimeRepository.java
@@ -1,2 +1,9 @@
-package com.devdojo.repository;public class AnimeRepository {
+package com.devdojo.repository;
+
+import com.devdojo.domain.Anime;
+
+import java.util.List;
+
+public interface AnimeRepository {
+    List<Anime> listAll();
 }
