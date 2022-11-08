0ddf0d14bc3f8fcfd702ef4c14d9218d54aba261
Luan Kenzley <luankenzley@hotmail.com>
2022-11-07 21:10:27 -0300

    adicionando novas validacoes

--- src/main/java/exception/BadRequestExceptionDetails.java
+++ src/main/java/exception/BadRequestExceptionDetails.java
@@ -1,16 +1,9 @@
 package exception;
 
-import lombok.Builder;
-import lombok.Data;
 
-import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
-@Data
-@Builder
-public class BadRequestExceptionDetails {
-    private String title;
-    private int status;
-    private String details;
-    private String developerMessage;
-    private LocalDateTime timestamp;
-}
+@Getter
+@SuperBuilder
+public class BadRequestExceptionDetails extends ExceptionDetails {}
