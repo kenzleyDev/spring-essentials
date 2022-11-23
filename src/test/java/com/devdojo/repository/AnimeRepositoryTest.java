d255f3bf5325633953633acf70749b9b7d1e9456
Luan Kenzley <kenzleydev@outlook.com>
2022-11-23 10:57:24 -0300

    iniciando testes unitarios

---
+++ src/test/java/com/devdojo/repository/AnimeRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.devdojo.repository;
+
+import com.devdojo.domain.Anime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@DisplayName("Tests for Anime Repository")
+class AnimeRepositoryTest {
+
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    @Test
+    @DisplayName("Save create anime when Successful")
+    void save_PersistAnime_WhenSuccessful() {
+        Anime animeToBeSaved = createAnime();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+
+        Assertions.assertThat(animeSaved).isNotNull();
+        Assertions.assertThat(animeSaved.getId()).isNotNull();
+        Assertions.assertThat(animeSaved.getName()).isEqualTo(animeToBeSaved.getName());
+
+    }
+
+    private Anime createAnime() {
+        return Anime.builder()
+                .name("Hajime no Ippo")
+                .build();
+    }
+
+}
